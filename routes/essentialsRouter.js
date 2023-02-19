66cbadd6845e93d10368ffcf7883cd379211648b
Adeeb Ogaili <adeebnasser@gmail.com>
2023-02-18 19:13:10 -0600

    added a post and get one functions to essentialsRouter

--- routes/essentialsRouter.js
+++ routes/essentialsRouter.js
@@ -13,7 +13,29 @@ essentialsRouter.get("/", (req, res, next) => {
     })
 })
 
+// Get one
+essentialsRouter.get("/:essentialId", (req, res, next) => {
+    Essentials.findOne({
+        _id: req.params.essentialId
+    }, (err, foundEssential) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(foundEssential)
+    })
+})
 
-
+// Poste one
+essentialsRouter.post("/", (req, res, next) => {
+    const newEssential = new Essentials(req.body)
+    newEssential.save((err, savedEssential) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedEssential)
+    })
+})
 
 module.exports = essentialsRouter
