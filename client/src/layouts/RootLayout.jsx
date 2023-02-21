8d4015bf85c849eb35c1319c3b1a2d66bd63a690
Adeeb Ogaili <adeebnasser@gmail.com>
2023-02-21 10:15:22 -0600

    added styles folder and fontawesome link

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import rootLayout from "../styles/rootLayout.css"
 
 const RootLayout = () => {
+
+  const [mobile, setMobile] = useState(false)
+
   return (
     <div className='root-layout'>
-        <header>
-          <nav>
+      <header>
+        <div className='container flexSB'>
+                <div className='logo flexSB'>
+                  
+                </div>
+            <nav className='flexSB'>
+                <ul className={mobile ? "navMenu-list" : 'flexSB'} onClick={() => setMobile(false)}>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/groceries">Groceries</NavLink>
                 <NavLink to="/kitchen">Kitchen</NavLink>
                 <NavLink to="/essentials">Essentials</NavLink>
-          </nav>
+                </ul>
+                <button className='toggle' onClick={() => setMobile(prevMobile => !prevMobile)}>
+                    {mobile ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
+                </button>
+            </nav>
+            
+        </div>
       </header>
       <main>
         <Outlet />
