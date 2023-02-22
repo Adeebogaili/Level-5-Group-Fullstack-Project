de68e96ab0b924326d9ee184dcb4f6a251f9edf3
PaulManRobertson <bigwallpaul514@gmail.com>
2023-02-22 12:53:41 -0700

    added groceries page and styling

--- client/src/pages/Groceries.jsx
+++ client/src/pages/Groceries.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AllGroceries from "../components/AllGroceries";
+import "../styles/groceries.css";
 
 const Groceries = () => {
-  return (
-    <div>Groceries</div>
-  )
-}
+  const [groceries, setGroceries] = useState([]);
 
-export default Groceries
+  const getGroceries = () => {
+    axios
+      .get("/groceries")
+      .then((res) => setGroceries(res.data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getGroceries();
+  }, []);
+
+  return (
+    <div className="groceries">
+      <h3>{`More than ${groceries.length} results for "groceries"`}</h3>
+      <div className="groceries-wrapper">
+        {groceries.map((groceries) => {
+          return (
+            <AllGroceries
+              key={groceries._id}
+              name={groceries.name}
+              description={groceries.description}
+              details={groceries.details}
+              newPrice={groceries.new_price}
+              oldPrice={groceries.old_price}
+              type={groceries.type}
+              imgUrl={groceries.imgUrl}
+              id={groceries._id}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Groceries;
