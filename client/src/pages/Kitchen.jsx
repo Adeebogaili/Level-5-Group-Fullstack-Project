be6c262a758191f61ff61b3ec45769503a872673
Cameron Ford <c.r.ford@outlook.com>
2023-02-22 14:18:00 -0700

    page working. useing css from allEssential.css

--- client/src/pages/Kitchen.jsx
+++ client/src/pages/Kitchen.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import AllKitchen from '../components/AllKitchen'
+import essential from "../styles/essentials.css"
 
 const Kitchen = () => {
 
-const [kitchenState, setKitchenState] = useState([])
+  const [kitchenState, setKitchenState] = useState([])
 
-  function getKitchen () {
+  const getKitchen = () => {
     axios
       .get("/kitchen")
       .then(res => setKitchenState(res.data))
@@ -16,11 +16,30 @@ const [kitchenState, setKitchenState] = useState([])
 
   useEffect(() => {
     getKitchen()
-    console.log(kitchenState)
   }, [])
 
   return (
-    <div>Kitchen</div>
+    <div className='essentials'>
+      <h3>{`More than ${kitchenState.length} results for "essentials"`}</h3>
+      <div className='essential-wrapper'>
+      {kitchenState.map(kitchenState => {
+        return (
+          <AllKitchen 
+            key={kitchenState._id}
+            name={kitchenState.name}
+            description={kitchenState.description}
+            details={kitchenState.details}
+            newPrice={kitchenState.new_price}
+            oldPrice={kitchenState.old_price}
+            type={kitchenState.type}
+            imgUrl={kitchenState.imgUrl}
+            id={kitchenState._id}
+          />
+        )
+      })}
+      </div>
+
+    </div>
   )
 }
 
