9db8a29b5f7965579a0d0629fce2b22dcc58b5b7
Cameron Ford <c.r.ford@outlook.com>
2023-02-22 13:24:53 -0700

    1st commit

--- client/src/pages/Kitchen.jsx
+++ client/src/pages/Kitchen.jsx
@@ -1,6 +1,24 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Kitchen = () => {
+
+const [kitchenState, setKitchenState] = useState([])
+
+  function getKitchen () {
+    axios
+      .get("/kitchen")
+      .then(res => setKitchenState(res.data))
+      .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    getKitchen()
+    console.log(kitchenState)
+  }, [])
+
   return (
     <div>Kitchen</div>
   )
