2fb3045e855c796f8d8707da265fb755479f4afe
PaulManRobertson <bigwallpaul514@gmail.com>
2023-02-24 13:52:47 -0700

    context to cart drop down

--- client/src/components/CartDropDown.jsx
+++ client/src/components/CartDropDown.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import CartItem from './CartItem'
+import {CartContext} from '../context'
 import "../styles/cartDropDown.css"
 
 const CartDropdown = (props) => {
+
+  const cart = useContext(CartContext)
+  
+  const productCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
         <CartItem />
-        <button>Checkout</button>
+        <button>Checkout ({productCount} Items)</button>
       </div>
     </div>
   );
--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {CartProvider} from "../context"
 
 const CartItem = () => {
   return (
-    <div className='cart-item'>CartItem</div>
+    <div className='cart-item'>{}</div>
   )
 }
 
