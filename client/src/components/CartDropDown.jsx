047763197a47b9b37847ac6116d4c1416c12b463
Cameron Ford <c.r.ford@outlook.com>
2023-02-25 01:55:01 -0700

    cleaned up code. there is a bug where react componets in cart render twice

--- client/src/components/CartDropDown.jsx
+++ client/src/components/CartDropDown.jsx
@@ -11,10 +11,6 @@ const CartDropdown = (props) => {
   const [essentials, setEssentials] = useState([])
   const [kitchenState, setKitchenState] = useState([])
   const [groceries, setGroceries] = useState([]);
-  const [filteredEssentials, setFilteredEssentials] = useState([])
-  const [filteredKitchen, setFilteredKitchen] = useState([])
-  const [filteredGroceries, setFilteredGroceries] = useState([])
-  const [num, setNum] = useState([0]);
 
   // set context
   const cart = useContext(CartContext);
@@ -25,84 +21,43 @@ const CartDropdown = (props) => {
     0
   );
 
-  //axios calls 
-  const getEssentials = () => {
-    axios
-        .get("/essentials")
-        .then(res => setEssentials(res.data))
-        .catch(err => console.error(err))
-  }
-
-  // const getGroceries = () => {
-  //   axios
-  //     .get("/groceries")
-  //     .then((res) => setGroceries(res.data))
-  //     .catch((err) => console.error(err));
-  // };
-
-  const getKitchen = () => {
-    axios
-      .get("/kitchen")
-      .then(res => setKitchenState(res.data))
-      .catch(err => console.error(err))
-  }
-
   useEffect(() => {
-    getEssentials()
-    // getGroceries()
-    getKitchen()
-    getGroceriesNew()
+    apiCall("essentials", setEssentials)
+    apiCall("groceries", setGroceries)
+    apiCall("kitchen", setKitchenState)
   }, [])
 
-
-  //main cart map
-  cart.items.map((product, index) => {
-  })  
-
-  const getGroceriesNew = () => {
+  function apiCall (url, setState) {
     axios
-      .get("/groceries")
+      .get(`/${url}`)
       .then((res) => 
       res.data.map((item) => {
-      cart.items.map((product) => {
-          // console.log(res.data)
-          // console.log(item._id)
-          // console.log(product.id)
-
+        cart.items.map((product) => {
           if (item._id === product.id) {
-            // console.log(item)
-            // setGroceries(item)
-            setGroceries((prevItems) => {
-              // console.log(item)
-              // console.log(prevItems)
+            setState((prevItems) => {
               return [
                 ...prevItems, item
-                // item
               ]
             })
-            // console.log(groceries)
           }
         })
       }))
-      .catch((err) => console.error(err));
+    .catch((err) => console.error(err));
   };
-
-  // console.log(groceries)
-  // console.log(groceries[0])
-  // console.log(groceries[0].name)
 
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
 
       {groceries.map(item => {
+        console.log(item._id)
         return(
-          <>
-            <h5>{item.name}</h5>
-          </>
+          <CartItem
+            item={item}
+            key={item._id}
+          />
         )
       })}
-
         <button>Checkout ({productCount} Items)</button>
       </div>
     </div>
--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import {CartProvider} from "../Context"
 
 export default function CartItem (props) {
-  // console.log(props)
   return (
-    <div className='cart-item'>{}</div>
+    <div className='cart-item'>{props.item.name}</div>
   )
 }
