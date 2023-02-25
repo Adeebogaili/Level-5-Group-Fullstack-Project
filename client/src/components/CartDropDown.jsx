84e8503a9b74ae01e8c5f879184c4fe03b73caf3
Cameron Ford <c.r.ford@outlook.com>
2023-02-24 18:54:43 -0700

    + and - working on all details pages

--- client/src/components/CartDropDown.jsx
+++ client/src/components/CartDropDown.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 // import CartItem from './CartItem'
-import { CartContext } from '../context'
+import { CartContext } from '../Context'
 import "../styles/cartDropDown.css";
 
 const CartDropdown = (props) => {
