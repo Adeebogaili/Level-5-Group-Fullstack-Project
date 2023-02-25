f1b8550eb912ddc7de9500eca63f04d2d7e28f87
Cameron Ford <c.r.ford@outlook.com>
2023-02-24 23:51:38 -0700

    data showing up on screen but the map seams to be repeating. there is 28 errors not sure why

--- client/src/components/CartDropDown.jsx
+++ client/src/components/CartDropDown.jsx
@@ -1,23 +1,30 @@
 import React, { useContext, useState, useEffect } from 'react'
 import axios from 'axios'
-// import CartItem from './CartItem'
+import CartItem from './CartItem'
 import { CartContext } from '../Context'
 import Essentials from '../pages/Essentials';
 import "../styles/cartDropDown.css";
+import { response } from 'express';
 
 const CartDropdown = (props) => {
+
+  //set state 
   const [essentials, setEssentials] = useState([])
   const [kitchenState, setKitchenState] = useState([])
   const [groceries, setGroceries] = useState([]);
+  const [filteredEssentials, setFilteredEssentials] = useState([])
+  const [filteredKitchen, setFilteredKitchen] = useState([])
+  const [filteredGroceries, setFilteredGroceries] = useState([])
+  const [num, setNum] = useState([0]);
 
+  // set context
   const cart = useContext(CartContext);
 
+  //show how many items in cart 
   const productCount = cart.items.reduce(
     (sum, product) => sum + product.quantity,
     0
   );
-
-  // console.log(cart.items)
 
   //axios calls 
   const getEssentials = () => {
@@ -27,12 +34,12 @@ const CartDropdown = (props) => {
         .catch(err => console.error(err))
   }
 
-  const getGroceries = () => {
-    axios
-      .get("/groceries")
-      .then((res) => setGroceries(res.data))
-      .catch((err) => console.error(err));
-  };
+  // const getGroceries = () => {
+  //   axios
+  //     .get("/groceries")
+  //     .then((res) => setGroceries(res.data))
+  //     .catch((err) => console.error(err));
+  // };
 
   const getKitchen = () => {
     axios
@@ -41,58 +48,110 @@ const CartDropdown = (props) => {
       .catch(err => console.error(err))
   }
 
-  
   useEffect(() => {
     getEssentials()
-    getGroceries()
+    // getGroceries()
     getKitchen()
+    getGroceriesNew()
   }, [])
   
-  // console.log(essentials)
+  // new var for mapped products
   let newEssentials = []
   let newGroceries = []
   let newKitchen = []
 
+  //main cart map
   cart.items.map((product, index) => {
-    // function findId (theId) {
-    //   return theId === product.id
+
+    //map function for products 
+    // function maping (theState, newArry) {
+    //   theState.map((item) => {
+    //     if (theState._id === product.id) {
+    //       const itemWithNum = {...item, quantity: product.quantity}
+    //       // console.log(product)
+    //       newArry.push(itemWithNum)
+    //       setNum((oldNum) => oldNum + 1)
+    //       // console.log(newArry)
+    //     }
+    //   })
     // }
-    // test = essentials.find(findId())
 
-    function maping (theState, newArry) {
-      theState.map((item) => {
-        if (theState._id === product.id) {
-          const itemWithNum = {...item, quantity: product.quantity}
-          console.log(product)
-          // newGroceries.push(groceries)
-          newArry.push(itemWithNum)
-          console.log(newArry)
-        }
-      })
-    }
+    
+    // function maping (theState, newArry) {
+      //   theState.map((item) => {
+        //     if (theState._id === product.id) {
+          //       const itemWithNum = {...item, quantity: product.quantity}
+          //       // console.log(product)
+          //       newArry.push(itemWithNum)
+          //       setNum((oldNum) => oldNum + 1)
+          //       // console.log(newArry)
+          //     }
+          //   })
+          // }
+          
+          //calling map function for proudocts 
 
-    maping(essentials, newEssentials)
-    maping(groceries, newGroceries)
-    maping(kitchenState, newKitchen)
+          // maping(essentials, newEssentials)
+          // maping(groceries, newGroceries)
+          // maping(kitchenState, newKitchen)
+        })  
 
-  })
+  const getGroceriesNew = () => {
+    axios
+      .get("/groceries")
+      .then((res) => 
+      cart.items.map((product) => {
+        res.data.map((item) => {
+          // console.log(res.data)
+          // console.log(item._id)
+          // console.log(product.id)
+
+          if (item._id === product.id) {
+            // console.log(item)
+            // setGroceries(item)
+            setGroceries((prevItems) => {
+              // console.log(item)
+              // console.log(prevItems)
+              return [
+                ...prevItems, item
+              ]
+            })
+            // console.log(groceries)
+          }
+        })
+      }))
+      .catch((err) => console.error(err));
+  };
+
+  // useEffect(() => {
+  //   // setFilteredEssentials((prevEssential) => {
+  //   //   return[...prevEssential, newEssentials]
+  //   // })
+  //   setFilteredEssentials(newEssentials)
+  //   console.log("hello world")
+  // }, num)
+
+  // console.log(filteredEssentials)
+  // console.log(num)
 
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
 
-        {/* {cart.items && cart.items.map && cart.items.map((product, index) => (
-          <div key={index}>
-            <h3>{product.name}</h3>
-          </div>
-        ))} */}
-
-        {cart.items.map((product, index) => (
-          <div key={index}>
-            {/* {console.log(product)} */}
-            <h3>{product.name}</h3>
-          </div>
-        ))}
+       {essentials.map(() => {
+        return (
+          <>
+          {groceries.map((item) => {
+            return (
+              <div>{item.new_price}</div>
+            )
+          })}
+            {/* <CartItem 
+              arryState={filteredEssentials}
+            /> */}
+          </>
+        )
+      })}
 
         <button>Checkout ({productCount} Items)</button>
       </div>
@@ -101,3 +160,10 @@ const CartDropdown = (props) => {
 };
 
 export default CartDropdown;
+
+// <CartItem 
+//   newArry={newGroceries}
+// />
+// <CartItem 
+//   newArry={newKitchen}
+// />
--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {CartProvider} from "../Context"
 
-const CartItem = () => {
+export default function CartItem (props) {
+  // console.log(props)
   return (
     <div className='cart-item'>{}</div>
   )
